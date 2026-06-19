6d428348d0ad569c321e04fafb262e8892403200
NF-Harris <ndrianamboarinaharris@gmail.com>
2026-06-20 00:57:06 +0300

    chore: rectification de securite

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)f=u%=sj8zpe_umb@wqe67u(s%h=86t36qv*%=x5q(1#xk+xzy'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'une-cle-de-secours-locale')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
